e18c532ce83cc2b848eb8acfae167d11e608d1db
Jeff Balogh <jbalogh@mozilla.com>
2012-02-16 16:24:45 -0800

    move the test helpers to an external file

---
+++ test/test-data/helpers.js
@@ -0,0 +1,45 @@
+var pass = 0,
+    fail = 0,
+    timeout = 0,
+    waiting = false;
+
+function assert(t) {
+  if (!!t) {
+    pass++;
+  } else {
+    fail++;
+    console.trace();
+  }
+}
+
+function done() {
+  if (waiting) return;
+
+  document.body.setAttribute('pass', pass);
+  document.body.setAttribute('fail', fail);
+  document.body.setAttribute('timeout', timeout);
+
+  var e = document.createEvent('Event');
+  e.initEvent('testDone', true, true);
+  document.dispatchEvent(e);
+}
+
+function wait(timeout) {
+  waiting = setTimeout(function() {
+    waiting = false;
+    timeout++;
+    done();
+  }, timeout || 10000);
+}
+
+function stopWaiting() {
+  clearTimeout(waiting);
+  done();
+}
+
+function tests(fun) {
+  document.addEventListener('DOMContentLoaded', function() {
+    fun();
+    done();
+  });
+}
